87565a26519d3aac2da19f9ae1499d080cd8bb90
Justin Carter <github@justicart.com>
2022-01-02 16:29:40 -0500

    christmas 2021

---
+++ src/routes/posts/2021-12-31-christmas-2021/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Christmas 2021`,
+  tags: ['christmas'],
+  spoiler: "This year we embarked on our biggest adventure yet...",
+  getContent: () => import('./document.mdx'),
+}

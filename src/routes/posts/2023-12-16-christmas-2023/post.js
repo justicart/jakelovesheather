84f5e89cab0f9beeae3dae4dd987e9869f648b44
Justin Carter <github@justicart.com>
2023-12-16 09:51:56 -0500

    Christmas 2023

---
+++ src/routes/posts/2023-12-16-christmas-2023/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Christmas 2023`,
+  tags: ['christmas'],
+  spoiler: "This year felt quieter and calmer than those before as we’ve settled into a bit of a routine. We are still living in SLC, working our same jobs, and enjoying every new phase with Max.",
+  getContent: () => import('./document.mdx'),
+}

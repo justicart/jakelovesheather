b58bbdc8c0b4df642dc6d0a42f9785dfbeb01333
Justin Carter <github@justicart.com>
2025-12-18 18:18:11 -0700

    2025 post

---
+++ src/routes/posts/2025-12-18-christmas-2025/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Christmas 2025`,
+  tags: ['christmas'],
+  spoiler: "2025 has been a challenging year for many, it seems.",
+  getContent: () => import('./document.mdx'),
+}
